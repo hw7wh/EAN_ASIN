d72cb43a394dce09e0a22444f567bce0a5c28354
hw7wh <hamzawahbi.pro@gmail.com>
2023-11-05 01:57:08 +0100

    Testing http clients for amazon : curl_cffi is great

--- test_amazon.py
+++ test_amazon.py
@@ -1,4 +1,8 @@
+# RUN AS STANDALONE TO TEST HTTP CLIENT FOR AMAZON : curl_cffi is great
+
 from curl_cffi.requests import Session
+import time
+import random
 
 USER_AGENT_LIST = [
     'chrome99',
@@ -14,16 +18,33 @@ USER_AGENT_LIST = [
     'safari15_5',
 ]
 
-URL = 'https://www.amazon.pl/dp/B0BPY7HSC4/'
+ASIN_LISTS = ['B000Z92N6K', 'B0BPY7HSC4']
+
+URL_LIST = [
+    'https://www.amazon.co.uk/dp/',
+    'https://www.amazon.pl/dp/',
+    'https://www.amazon.nl/dp/',
+    'https://www.amazon.it/dp/',
+    'https://www.amazon.fr/dp/',
+    'https://www.amazon.es/dp/',
+    'https://www.amazon.de/dp/',
+    'https://www.amazon.se/dp/',
+    'https://www.amazon.com.be/dp/',
+]
+
 
 impersonate = 'safari15_5'
 
 if __name__ == '__main__':
     try:
         with Session(impersonate=impersonate) as session:
-            url = f"{URL}"
-            response = session.get(url)
-            print(f'Connecting to {URL}...')
-            print(response.content)
+            for amazon_url in URL_LIST:
+                for asin in ASIN_LISTS:
+                    url = amazon_url + asin
+                    response = session.get(url)
+                    print(f'Connecting to {url}...')
+                    print(response.ok) # TRUE if prod exists FALSE otherwise
+                    print('________________________________________________________________\n\n\n\n')
+                    time.sleep(random.uniform(1,3))
     except Exception as e:
         print(f'Error : {e}')
